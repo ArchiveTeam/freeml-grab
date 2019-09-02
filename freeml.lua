1fb0baec3446f3a05555751447fa22595fbcffba
Matt Iggo <matt@iggo.co.uk>
2019-09-02 22:43:00 +0100

    Fix error on ban check (url should have been html) - Incapsula Incident ID - Request unsuccessful
    But this wouldn't have been an issue as the ban is only evident when "Incapsula incident ID" is served. Fixed and left for belt and braces approach.

--- freeml.lua
+++ freeml.lua
@@ -166,7 +166,7 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
       table.insert(urls, {url=match})
       addedtolist[match] = true
     end
-    if string.match(html, "Incapsula incident ID") or string.match(url, "Request unsuccessful.") then
+    if string.match(html, "Incapsula incident ID") or string.match(html, "Request unsuccessful.") then
       print("Incapsula blocked this pipeline'.")
       abortgrab = true
     end
